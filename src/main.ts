d9dea7c4f84c6c0ebea4249f0b98cb72313f435b
Vitaly Kolesnik <vitaly.kolesnik62@gmail.com>
2022-11-10 15:36:19 +0300

    Create NestJS project Blog with connect postgres via Typeorm

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ if (!process.env.IS_TS_NODE) {
 import { NestFactory } from '@nestjs/core';
 import { dataSource } from '@app/ormconfig';
 import { AppModule } from '@app/app/app.module';
-import { createDocumentation } from '@app/api/documentation';
+import { createDocumentation } from '@app/api/api.utils';
 
 async function bootstrap() {
   const port = process.env.PORT || 3000;
